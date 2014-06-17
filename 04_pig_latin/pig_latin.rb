9b16bed52d35ccabb4714d483d1a5d90a28088f3
Brandon Craft <brancraft@gmail.com>
2014-06-16 22:33:15 -0400

    efactoredray iglatinpay

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,9 +1,17 @@
-def translate(word)
-	if word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o" || word[0] == "u"
-		"#{word + word[0]}y"
-	elsif word[1] == "a" || word[1] == "e" || word[1] == "i" || word[1] == "o" || word[1] == "u"
-		"#{word[1..(word.size)] + word[0]}ay"
-	else 
-		"#{word[2..(word.size)] + word[0..1]}ay"	
+EXCEPTION = %w[ a e i o u ]
+
+def translate(phrase)
+	words = phrase.split(' ')
+	pig_words = []
+	words.each do |word|
+		if EXCEPTION.include?(word[0])
+			pig_words << "#{word}ay"
+
+		elsif EXCEPTION.include?(word[1])
+			pig_words << "#{word[1..-1] + word[0]}ay"
+		else
+			pig_words << "#{word[2..-1] + word[0..1]}ay"
+		end
 	end
+	pig_words.join(' ')
 end
